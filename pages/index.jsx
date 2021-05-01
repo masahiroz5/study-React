3476a305c03a20e3187058ddeac81fff14e74d3c
masahiroz5 <l.veg4sz5@gmail.com>
2021-05-01 15:33:23 +0900

    ⚗ クリックイベントの記述

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,14 +3,24 @@ import styles from "styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
+import { useCallback } from "react";
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo)
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>About Page</title>
       </Head>
       <Header />
+      <button onClick={handleClick}>ボタン</button>
       <Main page="index" />
       <Footer />
     </div>
